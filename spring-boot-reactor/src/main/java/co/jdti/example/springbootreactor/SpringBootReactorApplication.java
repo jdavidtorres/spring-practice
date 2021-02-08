90654ae5fcc92a4faeb581419f72e0ce8f9fe151
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-08 09:52:20 -0500

    Creación de proyecto

---
+++ spring-boot-reactor/src/main/java/co/jdti/example/springbootreactor/SpringBootReactorApplication.java
@@ -0,0 +1,13 @@
+package co.jdti.example.springbootreactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+}
