738f356f4cf96db1c6a8a8feea72666a141768aa
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-08 15:53:07 -0500

    Update Usuario.java

--- spring-boot-reactor/src/main/java/co/jdti/example/springbootreactor/models/Usuario.java
+++ spring-boot-reactor/src/main/java/co/jdti/example/springbootreactor/models/Usuario.java
@@ -7,4 +7,9 @@ public class Usuario {
 
     private String nombre;
     private String apellido;
+
+    public Usuario(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
 }
