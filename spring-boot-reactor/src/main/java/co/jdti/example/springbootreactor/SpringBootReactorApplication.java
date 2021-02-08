9cb8f98fc0f41673637ea81d7812a246718d4777
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-08 10:48:26 -0500

    Update SpringBootReactorApplication.java

--- spring-boot-reactor/src/main/java/co/jdti/example/springbootreactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/co/jdti/example/springbootreactor/SpringBootReactorApplication.java
@@ -1,13 +1,26 @@
 package co.jdti.example.springbootreactor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReactorApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactorApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Flux<String> nombres = Flux.just("Hugo", "Luis", "Paco")
+                // System.out::println tambien puede serr usario para minimizar codigo
+                .doOnNext(elemento -> System.out.println(elemento));
+        nombres.subscribe(log::info);
+    }
 }
