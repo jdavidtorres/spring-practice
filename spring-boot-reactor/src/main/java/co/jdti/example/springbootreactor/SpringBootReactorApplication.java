a701c5660338220129cdbc666e15266133b6ccca
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-11 15:43:01 -0500

    Se limpia el run
    Se limpia el run para organizar los ejemplos en metodos separados

--- spring-boot-reactor/src/main/java/co/jdti/example/springbootreactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/co/jdti/example/springbootreactor/SpringBootReactorApplication.java
@@ -22,6 +22,11 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        ejemploIterable();
+        ejemploFlatMap();
+    }
+
+    public void ejemploIterable() throws Exception {
         List<String> usuarios = new ArrayList<>();
         usuarios.add("Hugo Fulano");
         usuarios.add("Luis Mengano");
@@ -29,8 +34,38 @@ public class SpringBootReactorApplication implements CommandLineRunner {
         usuarios.add("Bruce Lee");
         usuarios.add("Bruce Willis");
 
-        Flux<Usuario> nombres = /*Flux.just("Hugo Fulano", "Luis Mengano", "Paco Zutano", "Bruce Lee", "Bruce Willis")*/
-        Flux.fromIterable(usuarios)
+        Flux<Usuario> nombres = Flux.fromIterable(usuarios)
+                .map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
+                .filter(usuario -> usuario.getNombre().equalsIgnoreCase("bruce"))
+                .doOnNext(usuario -> {
+                    if (usuario == null || usuario.getNombre().isEmpty()) {
+                        throw new RuntimeException("Nombres no pueden ser vacíos");
+                    } else {
+                        System.out.println(usuario.getNombre() + " " + usuario.getApellido());
+                    }
+                }).map(usuario -> {
+                    usuario.setNombre(usuario.getNombre().toLowerCase());
+                    usuario.setApellido(usuario.getApellido().toLowerCase());
+                    return usuario;
+                });
+        nombres.subscribe(us -> log.info(us.getNombre().concat(" ").concat(us.getApellido())), error -> log.error(error.getMessage()), new Runnable() {
+                    @Override
+                    public void run() {
+                        log.info("Se ha finalizado la ejecucion del observable con exito!");
+                    }
+                }
+        );
+    }
+
+    private void ejemploFlatMap() {
+        List<String> usuarios = new ArrayList<>();
+        usuarios.add("Hugo Fulano");
+        usuarios.add("Luis Mengano");
+        usuarios.add("Paco Zutano");
+        usuarios.add("Bruce Lee");
+        usuarios.add("Bruce Willis");
+
+        Flux<Usuario> nombres = Flux.fromIterable(usuarios)
                 .map(nombre -> new Usuario(nombre.split(" ")[0].toUpperCase(), nombre.split(" ")[1].toUpperCase()))
                 .filter(usuario -> usuario.getNombre().equalsIgnoreCase("bruce"))
                 .doOnNext(usuario -> {
