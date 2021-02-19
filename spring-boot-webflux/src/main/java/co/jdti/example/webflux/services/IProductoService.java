e025508ffa1954d206123f05e779489f5743dc1f
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-19 08:57:03 -0500

    Creamos el componente Service

---
+++ spring-boot-webflux/src/main/java/co/jdti/example/webflux/services/IProductoService.java
@@ -0,0 +1,20 @@
+package co.jdti.example.webflux.services;
+
+import co.jdti.example.webflux.models.documents.Producto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface IProductoService {
+
+    Flux<Producto> findAll();
+
+    Flux<Producto> findAllConNombreUpperCase();
+
+    Flux<Producto> findAllConNombreUpperCaseRepeat();
+
+    Mono<Producto> findById(String id);
+
+    Mono<Producto> save(Producto producto);
+
+    Mono<Void> delete(Producto producto);
+}
