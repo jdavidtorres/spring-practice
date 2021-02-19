c0e84cc7dab61138a04a12fa6b1e850cbab9cd2b
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-19 08:57:25 -0500

    Update IProductRepository.java

--- spring-boot-webflux/src/main/java/co/jdti/example/webflux/repositories/IProductRepository.java
+++ spring-boot-webflux/src/main/java/co/jdti/example/webflux/repositories/IProductRepository.java
@@ -2,6 +2,8 @@ package co.jdti.example.webflux.repositories;
 
 import co.jdti.example.webflux.models.documents.Producto;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IProductRepository extends ReactiveMongoRepository<Producto, String> {
 }
