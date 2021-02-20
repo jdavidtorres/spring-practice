222303e9d8bb788ee4d389886a5c5ed26467b60e
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-19 23:40:00 -0500

    Se agrega el campo categoria como seleccion en el formulario

--- spring-boot-webflux/src/main/java/co/jdti/example/webflux/controllers/ProductoContoller.java
+++ spring-boot-webflux/src/main/java/co/jdti/example/webflux/controllers/ProductoContoller.java
@@ -1,5 +1,6 @@
 package co.jdti.example.webflux.controllers;
 
+import co.jdti.example.webflux.models.documents.Categoria;
 import co.jdti.example.webflux.models.documents.Producto;
 import co.jdti.example.webflux.services.IProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
@@ -107,5 +109,10 @@ public class ProductoContoller {
         model.addAttribute("titulo", "Titulo");
         model.addAttribute("productos", productos);
         return "listar";
+    }
+
+    @ModelAttribute("categorias")
+    public Flux<Categoria> categorias() {
+        return iProductoService.findAllCategoria();
     }
 }
