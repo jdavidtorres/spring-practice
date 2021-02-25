532aa90c856943f1519a5faeffd918b906a92ea8
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-25 15:48:40 -0500

    Reciclamos codigo

--- spring-boot-apirest/src/main/java/co/jdti/example/springboot/app/SpringBootApirestApplication.java
+++ spring-boot-apirest/src/main/java/co/jdti/example/springboot/app/SpringBootApirestApplication.java
@@ -1,12 +1,60 @@
 package co.jdti.example.springboot.app;
 
+import co.jdti.example.springboot.app.models.documents.Categoria;
+import co.jdti.example.springboot.app.models.documents.Producto;
+import co.jdti.example.springboot.app.services.IProductoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import reactor.core.publisher.Flux;
+
+import java.util.Date;
 
 @SpringBootApplication
-public class SpringBootApirestApplication {
+public class SpringBootApirestApplication implements CommandLineRunner {
+
+    private final Logger log = LoggerFactory.getLogger(SpringBootApirestApplication.class);
+
+    @Autowired
+    private ReactiveMongoTemplate template;
+
+    @Autowired
+    private IProductoService iProductoService;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApirestApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        template.dropCollection(Producto.class).subscribe();
+        template.dropCollection(Categoria.class).subscribe();
+
+        Categoria electronico = new Categoria("Electrónico");
+        Categoria deporte = new Categoria("Deporte");
+        Categoria computacion = new Categoria("Computación");
+        Categoria muebles = new Categoria("Muebles");
+
+        Flux.just(electronico, deporte, computacion, muebles)
+                .flatMap(iProductoService::save)
+                .thenMany(Flux.just(new Producto("TV Panasocic Pantalla LCD", 456.89, electronico),
+                        new Producto("Camara Sony HD Digital", 177.89, electronico),
+                        new Producto("Apple iPod", 46.89, electronico),
+                        new Producto("Notebook Sony", 846.89, computacion),
+                        new Producto("Multifuncional Hewlett Packard", 200.89, computacion),
+                        new Producto("Bicicleta Bianchi", 70.89, deporte),
+                        new Producto("Notebook HP Omen 17", 2500.89, computacion),
+                        new Producto("Mica Cómoda 5 Cajones", 150.89, muebles),
+                        new Producto("TV Sony Bravia OLED 4K Ultra HD", 2255.89, electronico),
+                        new Producto("Telefono Samsung S10+", 400.00, electronico))
+                        .flatMap(producto -> {
+                            producto.setCreatedAt(new Date());
+                            return iProductoService.save(producto);
+                        }))
+                .subscribe();
     }
 }
---
+++ spring-boot-apirest/src/main/java/co/jdti/example/springboot/app/models/documents/Producto.java
@@ -0,0 +1,41 @@
+package co.jdti.example.springboot.app.models.documents;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "productos")
+public class Producto {
+
+    @Id
+    private String id;
+
+    @NotEmpty(message = "Es requerido")
+    private String nombre;
+
+    @NotNull
+    private Double precio;
+
+    @Valid
+    private Categoria categoria;
+
+    private String foto;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createdAt;
+
+    public Producto(String nombre, Double precio, Categoria categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+}
