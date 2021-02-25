81989c04b7bb88893f4166d2d2e19bb58a821a6f
Jesus Torres <jesu_ti91@hotmail.com>
2021-02-25 15:38:13 -0500

    Creando proyecto

---
+++ spring-boot-apirest/src/main/java/co/jdti/example/springboot/app/SpringBootApirestApplication.java
@@ -0,0 +1,12 @@
+package co.jdti.example.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApirestApplication.class, args);
+    }
+}
