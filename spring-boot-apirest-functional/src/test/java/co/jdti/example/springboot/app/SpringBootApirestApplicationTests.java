f51da25b0a13ffc68957b664e0dcbeced0383560
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-01 16:23:24 -0500

    Primer test

--- spring-boot-apirest-functional/src/test/java/co/jdti/example/springboot/app/SpringBootApirestApplicationTests.java
+++ spring-boot-apirest-functional/src/test/java/co/jdti/example/springboot/app/SpringBootApirestApplicationTests.java
@@ -1,13 +1,30 @@
 package co.jdti.example.springboot.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import co.jdti.example.springboot.app.models.documents.Producto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootApirestApplicationTests {
 
+    @Autowired
+    private WebTestClient client;
+
     @Test
-    void contextLoads() {
+    void listarTest() {
+        client.get()
+                .uri("/api/v3/productos")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON)
+                .expectBodyList(Producto.class)
+                .hasSize(10);
     }
 
 }
