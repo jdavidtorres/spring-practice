f83663ffaf341c099ae8acc026454a0a916a5bad
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-02 14:53:15 -0500

    Nuevo ejercicio

---
+++ spring-boot-client/src/main/java/co/jdti/example/client/app/SpringBootClientApplication.java
@@ -0,0 +1,13 @@
+package co.jdti.example.client.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootClientApplication.class, args);
+    }
+
+}
