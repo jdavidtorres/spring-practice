a62f806c4d5601a27fd31fcbdbb6b996abd0120a
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-03 12:42:39 -0500

    Update AppConfig.java

--- spring-boot-client/src/main/java/co/jdti/example/client/app/configurations/AppConfig.java
+++ spring-boot-client/src/main/java/co/jdti/example/client/app/configurations/AppConfig.java
@@ -1,5 +1,6 @@
 package co.jdti.example.client.app.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class AppConfig {
 
+    @Value("${config.base.endpoint}")
+    private String baseEndpoint;
+
     @Bean
     public WebClient registrarWebClient() {
-        return WebClient.create("http://localhost:8080/api/v3/productos");
+        return WebClient.create(baseEndpoint);
     }
 }
