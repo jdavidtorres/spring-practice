53a16296958a62225df7d522fce0da9436d69b02
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-05 07:56:28 -0500

    Nueva clase, full stack Spring y Angular

---
+++ microservicio-usuarios/src/main/java/co/jdti/example/microserviciousuarios/app/MicroservicioUsuariosApplication.java
@@ -0,0 +1,12 @@
+package co.jdti.example.microserviciousuarios.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioUsuariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioUsuariosApplication.class, args);
+    }
+}
