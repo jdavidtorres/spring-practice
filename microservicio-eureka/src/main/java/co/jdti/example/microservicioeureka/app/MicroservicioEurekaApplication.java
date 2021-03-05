f92edb67dc5bbc9f679614a70c96f55df119dd6c
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-05 15:09:53 -0500

    Servidor de nombres, Eureka Server

---
+++ microservicio-eureka/src/main/java/co/jdti/example/microservicioeureka/app/MicroservicioEurekaApplication.java
@@ -0,0 +1,12 @@
+package co.jdti.example.microservicioeureka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioEurekaApplication.class, args);
+    }
+}
