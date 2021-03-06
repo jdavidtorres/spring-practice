24daf43688887dd8cf61a945b29b464096708bfb
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-06 12:34:44 -0500

    Proyecto comun

---
+++ microservicio-commos/src/main/java/co/jdti/example/microserviciocommos/app/MicroservicioCommosApplication.java
@@ -0,0 +1,12 @@
+package co.jdti.example.microserviciocommos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCommosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioCommosApplication.class, args);
+    }
+}
