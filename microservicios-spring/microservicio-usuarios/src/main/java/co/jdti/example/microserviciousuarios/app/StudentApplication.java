92f08cf0ad1c8284993ffebfe1de026a9c40df96
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-08 16:35:23 -0500

    Modificando el EntityScan
    Con esta anotacion busca las Entidades en el proyecto common

--- microservicios-spring/microservicio-usuarios/src/main/java/co/jdti/example/microserviciousuarios/app/StudentApplication.java
+++ microservicios-spring/microservicio-usuarios/src/main/java/co/jdti/example/microserviciousuarios/app/StudentApplication.java
@@ -2,8 +2,10 @@ package co.jdti.example.microserviciousuarios.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EntityScan("co.jdti.example.microserviciocommons.models.entities")
 @EnableEurekaClient
 @SpringBootApplication
 public class StudentApplication {
