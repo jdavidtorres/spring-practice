43e77e2c8f37458e30c219efabbd23ec0b8d8726
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-08 16:49:01 -0500

    Agregando relación

--- microservicios-spring/microservicio-commons/src/main/java/co/jdti/example/microserviciocommons/models/entities/CourseEntity.java
+++ microservicios-spring/microservicio-commons/src/main/java/co/jdti/example/microserviciocommons/models/entities/CourseEntity.java
@@ -4,14 +4,18 @@ import lombok.Data;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 @Entity
@@ -30,8 +34,23 @@ public class CourseEntity {
     @Temporal(TemporalType.TIMESTAMP)
     private Date createAt;
 
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<StudentEntity> students;
+
+    public CourseEntity() {
+        this.students = new ArrayList<>();
+    }
+
     @PrePersist
     private void prePersist() {
         this.createAt = new Date();
+    }
+
+    public void addStudent(StudentEntity student) {
+        this.students.add(student);
+    }
+
+    public void removeStudent(StudentEntity student) {
+        this.students.remove(student);
     }
 }
