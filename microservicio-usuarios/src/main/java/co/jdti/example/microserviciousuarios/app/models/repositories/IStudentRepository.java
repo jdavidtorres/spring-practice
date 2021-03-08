62ead358a832ea1c831026028110ba1b742a00f2
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-07 19:39:03 -0500

    Eliminando carpeta

--- microservicio-usuarios/src/main/java/co/jdti/example/microserviciousuarios/app/models/repositories/IStudentRepository.java
+++
@@ -1,9 +0,0 @@
-package co.jdti.example.microserviciousuarios.app.models.repositories;
-
-import co.jdti.example.microserviciousuarios.app.models.entity.StudentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IStudentRepository extends JpaRepository<StudentEntity, Long> {
-}
