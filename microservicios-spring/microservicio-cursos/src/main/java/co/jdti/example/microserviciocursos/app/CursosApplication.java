1899388b396fe605afa0e7a48caace11048eef55
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-08 13:56:22 -0500

    Microservicio cursos

---
+++ microservicios-spring/microservicio-cursos/src/main/java/co/jdti/example/microserviciocursos/app/CursosApplication.java
@@ -0,0 +1,12 @@
+package co.jdti.example.microserviciocursos.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursosApplication.class, args);
+    }
+}
