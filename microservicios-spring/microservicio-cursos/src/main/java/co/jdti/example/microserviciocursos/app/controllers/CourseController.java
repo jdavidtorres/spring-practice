00e87846b93cacfbde13b7cc7070ca7b1ceee22e
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-09 10:29:05 -0500

    Asignar cursos y eliminar estudiante

--- microservicios-spring/microservicio-cursos/src/main/java/co/jdti/example/microserviciocursos/app/controllers/CourseController.java
+++ microservicios-spring/microservicio-cursos/src/main/java/co/jdti/example/microserviciocursos/app/controllers/CourseController.java
@@ -2,6 +2,7 @@ package co.jdti.example.microserviciocursos.app.controllers;
 
 import co.jdti.example.microserviciocommons.controllers.CommonController;
 import co.jdti.example.microserviciocommons.models.entities.CourseEntity;
+import co.jdti.example.microserviciocommons.models.entities.StudentEntity;
 import co.jdti.example.microserviciocursos.app.services.ICourseServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,5 +25,27 @@ public class CourseController extends CommonController<CourseEntity, ICourseServ
         }
         courseEntity.setId(id);
         return ResponseEntity.status(HttpStatus.OK).body(iServices.save(courseEntity));
+    }
+
+    @PutMapping("{id}/assign-students")
+    public ResponseEntity<?> assignStudents(@PathVariable Long id, @RequestBody List<StudentEntity> students) {
+        Optional<CourseEntity> obj = iServices.findById(id);
+        if (obj.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        CourseEntity course = obj.get();
+        students.forEach(course::addStudent);
+        return ResponseEntity.status(HttpStatus.OK).body(iServices.save(course));
+    }
+
+    @PutMapping("{id}/delete-student")
+    public ResponseEntity<?> deleteStudent(@PathVariable Long id, @RequestBody StudentEntity student) {
+        Optional<CourseEntity> obj = iServices.findById(id);
+        if (obj.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        CourseEntity course = obj.get();
+        course.removeStudent(student);
+        return ResponseEntity.status(HttpStatus.OK).body(iServices.save(course));
     }
 }
