7fe55e071e92099aa6714b4a88e84426ba652c46
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-10 12:58:55 -0500

    Update ExamController.java

--- microservicios-spring/microservicio-examenes/src/main/java/co/jdti/example/microservicioexamenes/app/controllers/ExamController.java
+++ microservicios-spring/microservicio-examenes/src/main/java/co/jdti/example/microservicioexamenes/app/controllers/ExamController.java
@@ -2,17 +2,39 @@ package co.jdti.example.microservicioexamenes.app.controllers;
 
 import co.jdti.example.microserviciocommons.controllers.CommonController;
 import co.jdti.example.microservicioexamenes.app.models.entity.ExamEntity;
+import co.jdti.example.microservicioexamenes.app.models.entity.QuestionEntity;
 import co.jdti.example.microservicioexamenes.app.services.IExamServices;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class ExamController extends CommonController<ExamEntity, IExamServices> {
-        return null;
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> edit(@RequestBody ExamEntity exam, @PathVariable Long id) {
+        Optional<ExamEntity> obj = iServices.findById(id);
+        if (obj.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        ExamEntity examDb = obj.get();
+        List<QuestionEntity> deleted = new ArrayList<>();
+        examDb.getQuestionsList().forEach(pdb -> {
+            if (!exam.getQuestionsList().contains(pdb)) {
+                deleted.add(pdb);
+            }
+        });
+        deleted.forEach(examDb::removeQuestion);
+
+        examDb.setQuestionsList(exam.getQuestionsList());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(iServices.save(examDb));
     }
 }
