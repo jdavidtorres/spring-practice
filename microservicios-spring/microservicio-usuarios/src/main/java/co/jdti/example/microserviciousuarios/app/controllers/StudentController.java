963a611d24efb0f589396dcf28defcfb6faa7bcc
Jesus Torres <jesu_ti91@hotmail.com>
2021-03-15 10:26:04 -0500

    Update StudentController.java

--- microservicios-spring/microservicio-usuarios/src/main/java/co/jdti/example/microserviciousuarios/app/controllers/StudentController.java
+++ microservicios-spring/microservicio-usuarios/src/main/java/co/jdti/example/microserviciousuarios/app/controllers/StudentController.java
@@ -3,7 +3,10 @@ package co.jdti.example.microserviciousuarios.app.controllers;
 import co.jdti.example.microserviciocommons.controllers.CommonController;
 import co.jdti.example.microserviciocommons.models.entities.StudentEntity;
 import co.jdti.example.microserviciousuarios.app.services.IStudentServices;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -49,7 +52,7 @@ public class StudentController extends CommonController<StudentEntity, IStudentS
     }
 
     @PutMapping("/edit-photo/{id}")
-    public ResponseEntity<?> editWithPhoto(@Valid @RequestBody StudentEntity student, BindingResult result, @PathVariable Long id, @RequestParam MultipartFile file) throws IOException {
+    public ResponseEntity<?> editWithPhoto(@Valid StudentEntity student, BindingResult result, @PathVariable Long id, @RequestParam MultipartFile file) throws IOException {
         if (result.hasErrors()) {
             return this.validator(result);
         }
@@ -62,5 +65,17 @@ public class StudentController extends CommonController<StudentEntity, IStudentS
             student.setPhoto(file.getBytes());
         }
         return ResponseEntity.status(HttpStatus.OK).body(iServices.save(student));
+    }
+
+    @GetMapping("/uploads/img/{id}")
+    public ResponseEntity<?> getPhoto(@PathVariable Long id) {
+        Optional<StudentEntity> obj = iServices.findById(id);
+        if (obj.isEmpty() || obj.get().getPhoto() == null) {
+            return ResponseEntity.noContent().build();
+        }
+        Resource img = new ByteArrayResource(obj.get().getPhoto());
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(img);
     }
 }
