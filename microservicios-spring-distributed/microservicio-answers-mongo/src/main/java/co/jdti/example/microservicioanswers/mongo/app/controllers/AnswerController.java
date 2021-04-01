c6288916894ce4b3b9465cf4a938600b2e9cc0d3
Jesus Torres <jesu_ti91@hotmail.com>
2021-04-01 09:40:26 -0500

    Cors

--- microservicios-spring-distributed/microservicio-answers-mongo/src/main/java/co/jdti/example/microservicioanswers/mongo/app/controllers/AnswerController.java
+++ microservicios-spring-distributed/microservicio-answers-mongo/src/main/java/co/jdti/example/microservicioanswers/mongo/app/controllers/AnswerController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "Content-Type", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
 public class AnswerController {
 
