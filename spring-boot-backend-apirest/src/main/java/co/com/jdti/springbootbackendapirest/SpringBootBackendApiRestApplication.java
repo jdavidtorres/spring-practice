3a1c168890b8e177b1b37b88953e1776e21954a9
Yisus <jesu_ti91@hotmail.com>
2022-06-14 16:56:23 -0500

    Update SpringBootBackendApiRestApplication.java

---
+++ spring-boot-backend-apirest/src/main/java/co/com/jdti/springbootbackendapirest/SpringBootBackendApiRestApplication.java
@@ -0,0 +1,34 @@
+package co.com.jdti.springbootbackendapirest;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import co.com.jdti.springbootbackendapirest.models.entities.Cliente;
+import co.com.jdti.springbootbackendapirest.models.repositories.IClienteDao;
+
+@SpringBootApplication
+public class SpringBootBackendApiRestApplication implements CommandLineRunner {
+
+	@Autowired
+	private IClienteDao iClienteDao;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApirestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		iClienteDao.save(
+				Cliente.builder().nombre("Jhon").apellido("Doe").email("doe@mail.com").createAt(new Date()).build());
+		iClienteDao.save(Cliente.builder().nombre("Linus").apellido("Trovals").email("doe@mail.com")
+				.createAt(new Date()).build());
+		iClienteDao.save(
+				Cliente.builder().nombre("Magma").apellido("Lee").email("doe@mail.com").createAt(new Date()).build());
+		iClienteDao.save(Cliente.builder().nombre("Jade").apellido("Leordof").email("doe@mail.com").createAt(new Date())
+				.build());
+	}
+}
--- spring-boot-backend-apirest/src/main/java/co/com/jdti/springbootbackendapirest/SpringBootBackendApirestApplication.java
+++ spring-boot-backend-apirest/src/main/java/co/com/jdti/springbootbackendapirest/SpringBootBackendApirestApplication.java
@@ -1,34 +1,34 @@
 package co.com.jdti.springbootbackendapirest;
 
-import java.util.Date;
-
+import co.com.jdti.springbootbackendapirest.models.entities.Cliente;
+import co.com.jdti.springbootbackendapirest.models.repositories.IClienteDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import co.com.jdti.springbootbackendapirest.models.entities.Cliente;
-import co.com.jdti.springbootbackendapirest.models.repositories.IClienteDao;
+import java.util.Date;
 
 @SpringBootApplication
-public class SpringBootBackendApirestApplication implements CommandLineRunner {
+public class SpringBootBackendApiRestApplication implements CommandLineRunner {
 
-	@Autowired
-	private IClienteDao iClienteDao;
+    @Autowired
+    private IClienteDao iClienteDao;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootBackendApirestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendApiRestApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		iClienteDao.save(
-				Cliente.builder().nombre("Jhon").apellido("Doe").email("doe@mail.com").createAt(new Date()).build());
-		iClienteDao.save(Cliente.builder().nombre("Linus").apellido("Trovals").email("doe@mail.com")
-				.createAt(new Date()).build());
-		iClienteDao.save(
-				Cliente.builder().nombre("Magma").apellido("Lee").email("doe@mail.com").createAt(new Date()).build());
-		iClienteDao.save(Cliente.builder().nombre("Jade").apellido("Leordof").email("doe@mail.com").createAt(new Date())
-				.build());
-	}
+    @Override
+    public void run(String... args) throws Exception {
+        iClienteDao.save(
+                Cliente.builder()
+                        .nombre("Jhon").apellido("Doe").email("doe@mail.com").createAt(new Date()).build());
+        iClienteDao.save(Cliente.builder().nombre("Linus").apellido("Trovals").email("doe@mail.com")
+                .createAt(new Date()).build());
+        iClienteDao.save(
+                Cliente.builder().nombre("Magma").apellido("Lee").email("doe@mail.com").createAt(new Date()).build());
+        iClienteDao.save(Cliente.builder().nombre("Jade").apellido("Leordof").email("doe@mail.com").createAt(new Date())
+                .build());
+    }
 }
