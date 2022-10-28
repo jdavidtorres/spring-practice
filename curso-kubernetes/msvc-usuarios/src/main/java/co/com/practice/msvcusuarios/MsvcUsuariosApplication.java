2cf398345a7d739ff549985d0006cfe298803688
jesustorres <jesu_ti91@hotmail.com>
2022-10-28 13:09:33 -0500

    Creando el proyecto

---
+++ curso-kubernetes/msvc-usuarios/src/main/java/co/com/practice/msvcusuarios/MsvcUsuariosApplication.java
@@ -0,0 +1,13 @@
+package co.com.practice.msvcusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcUsuariosApplication.class, args);
+	}
+
+}
---
+++ curso-kubernetes/src/main/java/co/com/practice/cursokubernetes/CursoKubernetesApplication.java
@@ -0,0 +1,12 @@
+package co.com.practice.cursokubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoKubernetesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoKubernetesApplication.class, args);
+	}
+}
