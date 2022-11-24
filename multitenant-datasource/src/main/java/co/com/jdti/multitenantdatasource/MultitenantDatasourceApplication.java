891175d3d3af664b66f9a8f01afcd873b3ac9354
Yisus <jesu_ti91@hotmail.com>
2022-11-24 12:09:45 -0500

    Proyecto en blanco

---
+++ multitenant-datasource/src/main/java/co/com/jdti/multitenantdatasource/MultitenantDatasourceApplication.java
@@ -0,0 +1,12 @@
+package co.com.jdti.multitenantdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultitenantDatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultitenantDatasourceApplication.class, args);
+	}
+}
