bd5bfb396ab7f289c406d54261d1c93fd3293d6f
Yisus <jesu_ti91@hotmail.com>
2023-02-02 11:38:35 -0500

    Creando proyecto

---
+++ spring-security/src/main/java/co/com/jdti/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package co.com.jdti.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+}
